b823d689935791ff8ed10caa23b9bdcea8345c95
federus1105 <federusrudi@gmail.com>
2025-11-25 13:27:39 +0700

    Feat ( index ) : feature main file for this project

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const authRoutes = require('./src/routes/auth.router');
-const productRoutes = require('./src/routes/products.router');
-const app = express();
-app.use(express.json());
+const router = require('./src/routes/index');
 
-app.use('/auth', authRoutes);
-app.use('/product', productRoutes);
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.use('/', router);
 
 app.get('/', (req, res)=>{
   res.json({
