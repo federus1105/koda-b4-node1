4f49e692a35d533ab61cbcb3e691c059c1189ffc
federus1105 <federusrudi@gmail.com>
2025-11-25 18:02:29 +0700

    Feat ( routes ) : init routes file upload for product

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const productControllers = require('../controllers/products.controllers');
 const router = express.Router();
+const upload = require('../libs/upload');
 
 router.get('/', productControllers.getListProduct);
 router.get('/:id', productControllers.getProductById);
 router.delete('/:id', productControllers.deleteproducbyId);
+router.post('/:id/upload', upload.single('image'), productControllers.uploadProductImage);
+
 
 module.exports = router
