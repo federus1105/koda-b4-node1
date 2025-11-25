5a69120f4a8a3fe068b3ac6c849d57710f760c82
federus1105 <federusrudi@gmail.com>
2025-11-25 22:42:32 +0700

    Feat ( routes ) : routes cerate product and update

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -6,6 +6,8 @@ const upload = require('../libs/upload');
 router.get('/', productControllers.getListProduct);
 router.get('/:id', productControllers.getProductById);
 router.delete('/:id', productControllers.deleteproducbyId);
+router.post('/', productControllers.addProduct)
+router.patch('/:id', productControllers.editProduct)
 router.post('/:id/upload', upload.single('image'), productControllers.uploadProductImage);
 
 
